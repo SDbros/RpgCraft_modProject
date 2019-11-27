1643fce695ecde3f6fd529098761663143ecee3c
Paul <zooiiooz@hotmail.com>
2019-11-27 20:11:28 +0100

    [added] Old zombie texture and runite armor texture

--- src/main/java/com/sdbros/rpgcraft/client/render/ZombieVariantModel.java
+++ src/main/java/com/sdbros/rpgcraft/client/render/ZombieVariantModel.java
@@ -2,6 +2,7 @@ package com.sdbros.rpgcraft.client.render;
 
 import com.sdbros.rpgcraft.entity.ZombieVariantEntity;
 import net.minecraft.client.renderer.entity.model.ZombieModel;
+import net.minecraft.client.renderer.entity.model.ZombieVillagerModel;
 
-class ZombieVariantModel extends ZombieModel<ZombieVariantEntity> {
+class ZombieVariantModel extends ZombieVillagerModel<ZombieVariantEntity> {
 }
