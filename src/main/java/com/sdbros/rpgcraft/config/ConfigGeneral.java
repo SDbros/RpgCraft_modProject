c8e0ed4e9d0a000b44e06e8ea9f8b3ac234e7fbf
Stijn <stijn.koster@mk18.nl>
2019-12-24 16:15:29 +0100

    [added] bad structure spawing
    Commit for back-up remove after

---
+++ src/main/java/com/sdbros/rpgcraft/config/ConfigGeneral.java
@@ -0,0 +1,20 @@
+package com.sdbros.rpgcraft.config;
+
+
+import com.sdbros.rpgcraft.RpgCraft;
+
+public class ConfigGeneral {
+
+	    public static boolean activateUpdateChecker = true;
+	    
+	    public static boolean activateDebugMode = false;
+	    
+	    public static boolean activateOverworldGeneration = true;
+	    
+	    public static boolean activateNetherGeneration = true;
+	    
+	    public static boolean activateEndGeneration = true;
+		
+		public static int[] additionalDimensions = new int [] {0};
+
+}
---
+++ src/main/java/com/sdbros/rpgcraft/structures/StructureList.java
@@ -0,0 +1,11 @@
+package com.sdbros.rpgcraft.structures;
+
+public class StructureList {
+		//Structures with an " _ " at the end of it's file name are special structures. (big structures that needs more space)
+	
+		//v.1.0
+		public static final StructureGenerator BROKEN_TOWER = new StructureGenerator("broken_tower");
+		public static final StructureGenerator BROKEN_BUILDING = new StructureGenerator("broken_building");
+		public static final StructureGenerator MAGIC_HOUSE = new StructureGenerator("magic_house");
+
+}
